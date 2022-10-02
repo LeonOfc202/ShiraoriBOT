d7fb453419291b30b00ae581f35eba182e686b32
LeonOfc202 <hhbb65202@gmail.com>
2022-10-02 10:45:01 +0800

    Create Ocheatall.js

---
+++ plugins/Ocheatall.js
@@ -0,0 +1,10 @@
+let { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender]
+        conn.reply(m.chat, `*Succes !*`, m)
+        global.db.data.users[m.sender].money = Infinity
+        global.db.data.users[m.sender].limit = Infinity
+        global.db.data.users[m.sender].level = Infinity
+        
+}
